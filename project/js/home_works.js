0dee172747f63e18e687144a8fb9a64ab57db059
elyyaaa <155648362+elyyaaa@users.noreply.github.com>
2024-01-13 15:17:09 +0600

    Add files via upload

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -1,3 +1,4 @@
+//hw1(part1)
 const gmailInput = document.querySelector("#gmail_input");
 const gmailButton = document.querySelector("#gmail_button");
 const gmailResult = document.querySelector("#gmail_result");
@@ -14,18 +15,88 @@ gmailButton.addEventListener("click",() =>{
 
 })
 
-
+// hw1(part2)
 const parentBlock = document.querySelector(".parent_block");
 const childBlock = document.querySelector(".child_block");
-const moveRight = (distance) => {
-    const currentPosition = parseInt(childBlock.style.left) || 0;
+// const moveRight = (distance) => {
+//     const currentPosition = parseInt(childBlock.style.left) || 0;
+//
+//     if (currentPosition < parentBlock.clientWidth - childBlock.clientWidth) {
+//         childBlock.style.left = `${currentPosition + distance}px`;
+//         setTimeout(() => moveRight(distance), 20);
+//     }
+// };
+// moveRight(2);
 
-    if (currentPosition < parentBlock.clientWidth - childBlock.clientWidth) {
-        childBlock.style.left = `${currentPosition + distance}px`;
-        setTimeout(() => moveRight(distance), 20);
+// hw2(part1)
+
+
+// requestAnimationFrame(() => moveRight(distance))
+
+const moveClockwise = () => {
+    const distance = 2;
+    const currentPositionX = parseInt(childBlock.style.left) || 0;
+    const currentPositionY = parseInt(childBlock.style.top) || 0;
+
+    if (
+        currentPositionX < parentBlock.clientWidth - childBlock.clientWidth &&
+        currentPositionY === 0
+    ) {
+        childBlock.style.left = `${currentPositionX + distance}px`;
+    } else if (
+        currentPositionX >= parentBlock.clientWidth - childBlock.clientWidth &&
+        currentPositionY < parentBlock.clientHeight - childBlock.clientHeight
+    ) {
+        childBlock.style.top = `${currentPositionY + distance}px`;
+    } else if (
+        currentPositionX > 0 &&
+        currentPositionY >= parentBlock.clientHeight - childBlock.clientHeight
+    ) {
+        childBlock.style.left = `${currentPositionX - distance}px`;
+    } else if (
+        currentPositionX === 0 &&
+        currentPositionY > 0
+    ) {
+        childBlock.style.top = `${currentPositionY - distance}px`;
     }
+
+    setTimeout(moveClockwise, 20);
 };
-moveRight(2);
+
+moveClockwise();
+
+// hw2(part2)
+
+let timerInterval;
+let seconds = 0;
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
+}
+
+function resetTimer() {
+    stopTimer();
+    seconds = 0;
+    updateTimer();
+}
+
+function updateTimer() {
+    document.getElementById('seconds').innerText = seconds++;
+}
+
+document.getElementById('start').addEventListener('click', startTimer);
+document.getElementById('stop').addEventListener('click', stopTimer);
+document.getElementById('reset').addEventListener('click', resetTimer);
+
+resetTimer();
+
+
+
+
 
 
 
